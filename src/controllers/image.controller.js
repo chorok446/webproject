f1d60021b4f4d3b9bf3c95c95d9fc01e2179e79d
Jeong Dong Won <chorok446@gmail.com>
2023-02-07 18:14:19 +0900

    refactor: 이미지 업로드 수정
    이미지 업로드에 있는 환경변수를 수정하고 조금더 가독성 있게 바꿨습니다

--- src/controllers/image.controller.js
+++ src/controllers/image.controller.js
@@ -1,19 +1,21 @@
 import {Router} from "express";
 import {upload} from "../middlewares/handler/image.handler";
 import {adminGuard, jwtGuard} from "../middlewares";
-
+import {bucket, region} from "../config/aws.config";
 
 const imageController = Router();
-const aws_url = process.env.AWS_S3_URL
+// aws 버킷 도메인
+const aws_url = `https://${bucket}.s3.${region}.amazonaws.com/`
+
 
 //로그인 및 관리자만 접근가능
 imageController.post("/", jwtGuard, adminGuard, upload.array('images'), async (req, res, next) => {
-        try {
-            // 버킷에 업로드된 파일 도메인 연결
-            const imagePath = req.files.map((image) => `${aws_url}${image.key}`);
+    try {
+        // 버킷에 업로드된 파일 도메인 연결
+        const imagePath = req.files.map((image) => `${aws_url}${image.key}`);
 
-            res.status(200).json(imagePath);
-        } catch (error) {
+        res.status(200).json(imagePath);
+    } catch (error) {
             next(error);
         }
     }
