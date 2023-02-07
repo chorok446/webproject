85ed3dbfafda351ce5a2f40682f665fc0ab561b5
Jeong Dong Won <chorok446@gmail.com>
2023-02-07 18:09:30 +0900

    feat: 로깅 기능 추가
    morgan과 winston을 사용해서 서버에 로그를 남기는 기능을 추가했습니다.

---
+++ src/middlewares/logger/morganMiddleware.js
@@ -0,0 +1,32 @@
+import morgan from 'morgan';
+import logger from './config/logger';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const format = () => {
+    const result = process.env.NODE_ENV === 'production' ? 'combined' : 'dev';
+    return result;
+};
+
+
+const stream = {
+    write: (message) => {
+        logger.http(
+            message.replace(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g, "")
+        );
+    },
+};
+
+
+const skip = (_, res) => {
+    if (process.env.NODE_ENV === 'production') {
+        return res.statusCode < 400;
+    }
+    return false;
+};
+
+//? 적용될 moran 미들웨어 형태
+const morganMiddleware = morgan(format(), {stream, skip});
+
+export default morganMiddleware;
